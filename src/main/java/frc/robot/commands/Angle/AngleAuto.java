bb36c89eaa9480f22a082076cbddd60b836d27e8
Sz-Wei Chen <swchen1217@gmail.com>
2025-02-13 19:42:30 +0800

    format

--- src/main/java/frc/robot/commands/Angle/AngleAuto.java
+++ src/main/java/frc/robot/commands/Angle/AngleAuto.java
@@ -4,7 +4,6 @@
 
 package frc.robot.commands.Angle;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.AlgaeGrabberSubsystem;
 import frc.robot.subsystems.CoralGrabberSubsystem;
@@ -48,59 +47,50 @@ public class AngleAuto extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
-
-    if(elevatorState == ElevatorState.kL1 && algaeState == AlgaeState.kAlgaeTop){ //L1
+    if (elevatorState == ElevatorState.kL1 && algaeState == AlgaeState.kAlgaeTop) { // L1
       elevatorSubsystem.setL1();
       algaeGrabberSubsystem.setAlgaeTopPosition();
-      if(coralState == CoralState.kL1){
+      if (coralState == CoralState.kL1) {
         coralGrabberSubsystem.setL1Position();
       }
     }
-    if(elevatorState == ElevatorState.kL2 && algaeState == AlgaeState.kAlgaeTop){ //L2
+    if (elevatorState == ElevatorState.kL2 && algaeState == AlgaeState.kAlgaeTop) { // L2
       elevatorSubsystem.setL2();
       algaeGrabberSubsystem.setAlgaeTopPosition();
-      if(coralState == CoralState.kL2){
+      if (coralState == CoralState.kL2) {
         coralGrabberSubsystem.setL1Position();
       }
     }
-    if(elevatorState == ElevatorState.kTop && algaeState == AlgaeState.kAlgaeTop){ //L3
-      elevatorSubsystem.setTop();
+    if (elevatorState == ElevatorState.kL3 && algaeState == AlgaeState.kAlgaeTop) { // L3
+      elevatorSubsystem.setL3();
       algaeGrabberSubsystem.setAlgaeTopPosition();
-      if(coralState == CoralState.kL3){
+      if (coralState == CoralState.kL3) {
         coralGrabberSubsystem.setL3Position();
       }
     }
-    if(elevatorState == ElevatorState.kTop && algaeState == AlgaeState.kAlgaeTop){ //L4
-      elevatorSubsystem.setTop();
+    if (elevatorState == ElevatorState.kL4 && algaeState == AlgaeState.kAlgaeTop) { // L4
+      elevatorSubsystem.setL4();
       algaeGrabberSubsystem.setAlgaeTopPosition();
-      if(coralState == CoralState.kCoralTop){
+      if (coralState == CoralState.kCoralTop) {
         coralGrabberSubsystem.setCoralTopPosition();
       }
     }
-    if(elevatorState == ElevatorState.kDefault && algaeState == AlgaeState.kAlgaePutPro){ //processor
+    if (elevatorState == ElevatorState.kDefault && algaeState == AlgaeState.kAlgaePutPro) { // processor
       elevatorSubsystem.setDefault();
       algaeGrabberSubsystem.setAlgaePutProPosition();
-      if(coralState == CoralState.kCoralTop){
+      if (coralState == CoralState.kCoralTop) {
         coralGrabberSubsystem.setCoralTopPosition();
       }
     }
-
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    
-
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    
-    
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
