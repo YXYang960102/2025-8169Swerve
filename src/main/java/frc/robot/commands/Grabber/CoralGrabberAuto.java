189de253e2d36a3c8faa4b76dc285ca008f8a0e5
Sz-Wei Chen <swchen1217@gmail.com>
2025-02-23 08:19:09 +0800

    feat: coral auto

---
+++ src/main/java/frc/robot/commands/Grabber/CoralGrabberAuto.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Grabber;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.CoralGrabberConstants.CoralGrabberAction;
+import frc.robot.subsystems.CoralGrabberSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class CoralGrabberAuto extends Command {
+  private CoralGrabberSubsystem coralGrabberSubsystem;
+
+  /** Creates a new CoralGrabberAutoLoad. */
+  public CoralGrabberAuto(CoralGrabberSubsystem coralGrabberSubsystem) {
+    this.coralGrabberSubsystem = coralGrabberSubsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+
+    addRequirements(coralGrabberSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    coralGrabberSubsystem.setGrabberAction(CoralGrabberAction.kRevAuto);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    coralGrabberSubsystem.setGrabberAction(CoralGrabberAction.kStop);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return coralGrabberSubsystem.getSensor();
+  }
+}
